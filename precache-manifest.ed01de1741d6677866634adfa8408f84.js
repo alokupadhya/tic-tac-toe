d103da0aa7d23c331d21e4f5fb94c224ab82bbbc
Alok Upadhya <alokupadhya0@gmail.com>
2020-06-08 17:37:13 +0530

    Updates

---
+++ precache-manifest.ed01de1741d6677866634adfa8408f84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf1cc3b532589683905d94227180e98",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b96dd44844d0bea9f233",
+    "url": "/tic-tac-toe/static/js/2.93ec7896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.93ec7896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43aacc6f59fa5136cddb",
+    "url": "/tic-tac-toe/static/js/main.cb7a3280.chunk.js"
+  },
+  {
+    "revision": "ecbc6f5089b996663329",
+    "url": "/tic-tac-toe/static/js/runtime-main.a982f7cd.js"
+  }
+]);
